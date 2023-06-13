9ff06196301067cc894877954244fbb13e1e883f
jupiter229 <smartwang1004@outlook.com>
2023-06-12 20:53:00 -0700

    feat: add typescript code to server

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,11 +1,13 @@
-type Post = {
+type IPost = {
   slug: string;
   title: string;
 }
 
+import type { Post } from "@prisma/client";
+
 import { prisma } from "~/db.server";
 
-export async function getPosts(): Promise<Array<Post>> {
+export async function getPosts(): Promise<Array<IPost>> {
   return prisma.post.findMany();
 }
 
@@ -13,7 +15,8 @@ export async function getPost(slug: string) {
   return prisma.post.findUnique({ where: { slug }});
 }
 
-export async function createPost(post) {
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
   return prisma.post.create({ data: post });
-  
 }
